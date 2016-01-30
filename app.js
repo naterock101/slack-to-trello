1e165240bf9e010796f3ed8668a50ab7a2cff51e
Nate Dawg <Nathan.levine@gmail.com>
2016-01-29 23:56:43 -0800

    just a test

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var idMembers = null;
 
 function postToTrello(listId, command, text, user_name, cb) {
   if (text == undefined || text == null || text == "") {
@@ -18,29 +17,30 @@ function postToTrello(listId, command, text, user_name, cb) {
   var name_and_desc = text.split('|');
 
   //stupid hack for injecting member names
-  if (name_and_desc[1] !== null) {
-    idMembers = name_and_desc[1].replace(/\s+/g, '');
-  };
+  // var idMembers;
+  // if (name_and_desc[1] == undefined || name_and_desc[1] == null || name_and_desc[1] == "") {
+  //   idMembers = null;
+  // else {
+  //   idMembers = name_and_desc[1].replace(/\s+/g, '');
+  // }  
 
-  if
-
-  if (idMembers == "nate" || idMembers == "nathan") {
-    idMembers = "51902656858a2d1d2400371f";
-  } else if (idMembers == "brac") {
-    idMembers = "4fe08f7a29951994158d3359";
-  } else if (idMembers == "zern" || idMembers == "zernyu") {
-    idMembers = "520e839359eba5b04a004244";
-  } else if (idMembers == "gabe" || idMembers == "gabriel") {
-    idMembers = "52d9e572ab5bf0847717c4b1";
-  } else if (idMembers == "garrett" || idMembers == "garett" || idMembers == "garret" || idMembers == "garet") {
-    idMembers = "52fbef485146d913398cb95a";
-  } else if (idMembers == "caroline") {
-    idMembers = "532897b3dda5d9550f86e39b";
-  } else if (idMembers == "dan") {
-    idMembers = "5580a1b779d64277225f6c39";
-  } else if (idMembers == "harris") {
-    idMembers = "52032ab362af020708000872";
-  };
+  // if (idMembers == "nate" || idMembers == "nathan") {
+  //   idMembers = "51902656858a2d1d2400371f";
+  // } else if (idMembers == "brac") {
+  //   idMembers = "4fe08f7a29951994158d3359";
+  // } else if (idMembers == "zern" || idMembers == "zernyu") {
+  //   idMembers = "520e839359eba5b04a004244";
+  // } else if (idMembers == "gabe" || idMembers == "gabriel") {
+  //   idMembers = "52d9e572ab5bf0847717c4b1";
+  // } else if (idMembers == "garrett" || idMembers == "garett" || idMembers == "garret" || idMembers == "garet") {
+  //   idMembers = "52fbef485146d913398cb95a";
+  // } else if (idMembers == "caroline") {
+  //   idMembers = "532897b3dda5d9550f86e39b";
+  // } else if (idMembers == "dan") {
+  //   idMembers = "5580a1b779d64277225f6c39";
+  // } else if (idMembers == "harris") {
+  //   idMembers = "52032ab362af020708000872";
+  // };
 
   // //stupid hack for injecting labels
   // var idLabelColor = null;
@@ -75,7 +75,7 @@ function postToTrello(listId, command, text, user_name, cb) {
 
   var card_data = {
     'name' : name_and_desc.shift() + ' (@' + user_name + ')',
-    'idMembers' : idMembers,
+    'idMembers' : null,
     'idLabels' : null,
     'desc' : null,
     'due' : null,
@@ -94,7 +94,7 @@ app.post('/*', function(req, res, next) {
   user_name = req.body.user_name;
 
   postToTrello(listId, command, text, user_name, function(err, data) {
-    //if (err) throw err;
+    if (err) throw err;
     console.log(data);
 
     var name = data.name;
