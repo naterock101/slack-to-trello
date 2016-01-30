bb5f8a7861011a1aa2082b0414a18194356d5ab4
Nate Dawg <Nathan.levine@gmail.com>
2016-01-29 23:59:24 -0800

    am i a huge dummy?

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ function postToTrello(listId, command, text, user_name, cb) {
   // };
 
   var card_data = {
-    'name' : name_and_desc.shift() + ' (@' + user_name + ')',
+    'name' : name_and_desc + ' (@' + user_name + ')',
     'idMembers' : null,
     'idLabels' : null,
     'desc' : null,
